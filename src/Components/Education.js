1352d981af15c49cd7f134c8fa4b318d3af30f6e
Debbie Wright <debbiewright@live.co.uk>
2022-10-26 11:40:41 +0100

    Addition of education section

---
+++ src/Components/Education.js
@@ -0,0 +1,125 @@
+
+import React from "react";
+import uniqid from "uniqid";
+import addIcon from '../Icons/add.png';
+import deleteIcon from '../Icons/delete.png';
+
+
+
+export default function Education() {
+    const [education, setEducation] = React.useState({
+        ed: {
+            id: uniqid(),
+            school: "",
+            qualification: "",
+            grade: "",
+            dateAchieved: ""
+        },
+        educations: [],
+        previewDisplay: true
+    })
+
+
+    function handleAdd(){
+        setEducation(prevEducation => ({
+            ...prevEducation,
+            previewDisplay: false
+        }))
+    }
+
+    function handleSubmit(event){
+        event.preventDefault();
+        setEducation(prevEducation => ({
+            educations: prevEducation.educations.concat(prevEducation.ed),
+            ed: {
+                id: uniqid(),
+                school: "",
+                qualification: "",
+                grade: "",
+                dateAchieved: ""
+            },
+            previewDisplay: true
+        }))
+    }
+    
+
+    function handleChange(event){
+        const{name, value} = event.target;
+        console.log("changing: " + [name] + " to " + value);
+        setEducation(prevEducation => ({
+            ...prevEducation,
+            ed:{
+                ...prevEducation.ed,
+                [name]: value
+            }
+        }))
+    }
+
+    function handleDelete(event){
+        let educationId = event.target.id;
+        setEducation(prevEducation => ({
+            ...prevEducation,
+            educations: prevEducation.educations.filter(ed => ed.id !== educationId)
+        }))
+    }
+     
+    return (
+            
+            <div id="educations">
+                <div id="educationsHeadingDiv">
+                    <h2 className="underline">Education</h2>
+                    <img src={addIcon} alt="Add" className="addButton" id="addEducationButton" onClick={handleAdd}/>
+                </div>
+                {education.educations && 
+                    <div id="educationItems">
+                        
+                            {console.log(JSON.stringify(education))}
+                            {education.educations.map((ed)=>{
+                                return <div className="educationItemDiv" key={ed.id}>{
+                                }<p>School: </p><p className="italics">{ed.school}</p>
+                                 <img src={deleteIcon} alt="Delete" className="deleteButton" id={ed.id} onClick={handleDelete}/>
+                                <p>Qualification: </p><p className="edItem italics">{ed.qualification}</p>
+                                <p>Grade: </p><p className="edItem italics">{ed.grade}</p>
+                                <p>Date Achieved: </p><p className="edItem italics">{ed.dateAchieved}</p>
+                                <hr/>
+                           </div>
+                                
+                            })}
+                    </div>
+                }
+                {!education.previewDisplay && 
+                        <form id="educationForm" className ="form" onSubmit={handleSubmit}>
+                            School/College: <input id="editSchoolField"
+                                type="text"
+                                name="school"
+                                onChange={handleChange}
+                                value={education.ed.school}
+                            />
+                            Qualification: <input id="editQualificationField"
+                                type="text"
+                                name="qualification"
+                                onChange={handleChange}
+                                value={education.ed.qualification}
+                            />
+                            Grade: <input id="editGradeField"
+                                type="text"
+                                name="grade"
+                                onChange={handleChange}
+                                value={education.ed.grade}
+                            />
+                             Year Achieved: <input id="editYearField"
+                                type="date"
+                                name="dateAchieved"
+                                onChange={handleChange}
+                                value={education.ed.dateAchieved}
+                            />
+                            <button className="form-submit" id="addEducationItemButton">Add</button>
+                        </form>
+                }
+
+            </div>
+        );
+    
+
+}
+
--- src/Components/RightPanel.js
+++ src/Components/RightPanel.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import Intro from "./Intro";
+import Education from "./Education";
 
 const RightPanel = () => {
   return (
     <div id="rightPanel">
         <Header />
         <Intro />
+        <Education />
     </div>
   );
 };
